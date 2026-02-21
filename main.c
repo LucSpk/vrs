62b579cbe516a2caffc1d8f208b8809985965d73
Lucas Alves <olalvezluc@gmail.com>
2026-02-20 22:31:33 -0300

    Retorna os arquivos para main e imprime

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void identificaArquivos(char *caminho, int exibeOcultos, char ***arr); 
+void identificaArquivos(char *caminho, int exibeOcultos, char ***arr, int *tamanhoFinal); 
 
 int main() {
     printf("Iniciando...\n");
 
     char **filePaths = NULL;          // - Um ponteiro para um ponteiro de char
-    identificaArquivos(".", 0, &filePaths);   
+    int totalArquivos = 0;
+    identificaArquivos(".", 0, &filePaths, &totalArquivos); 
+
+    printf("\nArquivos encontrados: %d\n\n", totalArquivos);
+
+    for (int i = 0; i < totalArquivos; i++) {
+        printf("%s\n", filePaths[i]);
+        free(filePaths[i]);   // libera cada string
+    }
+
+    free(filePaths);          // libera o array principal
 
     return 0;
 }
