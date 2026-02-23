9c8e016b2db18fdcb83d85b5552534d41afef898
Lucas Alves <olalvezluc@gmail.com>
2026-02-23 20:24:57 -0300

    Evitar continuidade do codigo, caso erro

--- src/rastreador/leitor_de_arquivos/le_arquivo.c
+++ src/rastreador/leitor_de_arquivos/le_arquivo.c
@@ -6,6 +6,7 @@ static void _le_conteudo_arquivo(char *filePath) {
     ptr_arquivo = fopen(filePath, "r");
     if(ptr_arquivo == NULL) {
         printf("ERRO! O arquivo não foi aberto!\n");
+        return;
     }
     printf("Sucesso! O arquivo foi aberto!\n");
 
