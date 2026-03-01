ed5daa9980c8fbc349a6bdf895c082d06cc993ff
Lucas Alves <olalvezluc@gmail.com>
2026-03-01 02:58:20 -0300

    Implementa função para salvar arquivo em diretório com criação de diretórios necessários

---
+++ src/arquivo/salva_arquivo.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <linux/limits.h>
+
+static void _cria_diretorios(const char *path) {
+    char tmp[PATH_MAX];
+    snprintf(tmp, sizeof(tmp), "%s", path);
+
+    for (char *p = tmp + 1; *p; p++) {
+        if (*p == '/') {
+            *p = '\0';
+            mkdir(tmp, 0777);
+            *p = '/';
+        }
+    }
+
+    mkdir(tmp, 0777);
+}
+
+static void _salva_arquivo_no_diretorio(const char *caminho, const char *fileName, const char *zipFile) {
+    _cria_diretorios(caminho);
+    
+    int tamanho = strlen(caminho) + strlen(fileName) + 2;
+    char filePath[tamanho];
+    
+    snprintf(filePath, sizeof(filePath), "%s/%s", caminho, fileName);
+    
+    printf("Criando: %s\n", filePath);
+    FILE *arquivo;
+    arquivo = fopen(filePath, "wb");
+    if (arquivo == NULL) {
+        printf("Erro ao criar o arquivo!\n");
+        return;
+    }
+
+    fprintf(arquivo, zipFile);
+    fclose(arquivo);
+}
+
+void salva_arquivo_no_diretorio(const char *caminho, const char *fileName, const char *zipFile) {
+    _salva_arquivo_no_diretorio(caminho, fileName, zipFile);
+}
