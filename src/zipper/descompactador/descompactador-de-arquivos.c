0cc579bdc69967c83455eb5d6933cec6f95b5488
Lucas Alves <olalvezluc@gmail.com>
2026-03-03 20:25:10 -0300

    Implementa função de descompactação de arquivos utilizando ZipperFile

--- src/zipper/descompactador/descompactador-de-arquivos.c
+++ src/zipper/descompactador/descompactador-de-arquivos.c
@@ -1,8 +1,27 @@
-static char *_descompacta_arquivos(const char *arquivoZip) {
-    
+#include <stdlib.h>
+#include <zlib.h>
+#include <string.h>
+#include "../zipperfile.h"
 
+static char *_descompacta_arquivos(const ZipperFile arquivoZip) {
+    
+    uLongf destLen = (uLongf)arquivoZip.tamanhDescomprimido;
+    unsigned char *uncompressFille = malloc(destLen);
+
+    int res = uncompress(
+        (Bytef *)uncompressFille,
+        &destLen,
+        (const Bytef *)arquivoZip.conteudoComprimido,
+        (uLongf)arquivoZip.tamanhoComprimido
+    );
+
+    if(res == Z_OK) 
+        return (char *)uncompressFille;
+    
+    free(uncompressFille);
+    return "";
 }
 
-char *descompacta_arquivos(const char *arquivoZip) {
+char *descompacta_arquivos(const ZipperFile arquivoZip) {
     return _descompacta_arquivos(arquivoZip);
 }
