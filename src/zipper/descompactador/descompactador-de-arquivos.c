0c5b44684bcd035893668a6cc29d95d4eaa97db3
Lucas Alves <olalvezluc@gmail.com>
2026-03-05 21:08:32 -0300

    Inicia lógica para fazer o uncompress sem saber o tamanho final, usando o inflateEnd da biblioteca zlib, essa lógica usa chunks para descompactar

--- src/zipper/descompactador/descompactador-de-arquivos.c
+++ src/zipper/descompactador/descompactador-de-arquivos.c
@@ -5,6 +5,23 @@
 
 static char *_descompacta_arquivos(const ZipperFile arquivoZip) {
     
+    z_stream stream;
+
+    int err;
+    err = inflateInit(&stream);
+    if(err) {
+        printf("Erro ao iniciar o inflate!");
+        return "";
+    }
+
+    stream.next_in = (Bytef *)arquivoZip.conteudoComprimido;
+    stream.next_in = (uInt)arquivoZip.tamanhoComprimido;
+
+    size_t buffer_size = 1024;
+    unsigned char *buffer = malloc(buffer_size);
+
+    
+
     uLongf destLen = (uLongf)arquivoZip.tamanhDescomprimido;
     unsigned char *uncompressFille = malloc(destLen);
 
