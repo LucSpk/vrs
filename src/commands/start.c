c97b3a3a8f133ab0cb89cdedbcfa6373443dabfc
Lucas Alves <olalvezluc@gmail.com>
2026-03-11 19:24:46 -0300

    Cria diretorios

--- src/commands/start.c
+++ src/commands/start.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include "start.h"
+
+static int cria_diretorio(const char *nome)
+{
+    if (mkdir(nome, 0777) == 0) {
+        printf("Diretório criado: %s\n", nome);
+        return 0;
+    }
+
+    printf("Diretório já existe ou erro ao criar: %s\n", nome);
+    return 1;
+}
 
 int command_start(void)
 {
     printf("Inicializando repositório VSR...\n");
 
+    cria_diretorio(".vsr");
+    cria_diretorio(".vsr/objects");
 
     printf("Repositório iniciado com sucesso.\n");
 
