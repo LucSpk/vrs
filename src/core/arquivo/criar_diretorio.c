d3864bcce376d0fa1e2e7b586a97934407b4c87b
Lucas Alves <olalvezluc@gmail.com>
2026-03-14 23:36:47 -0300

    Faz implementação da funcao

--- src/core/arquivo/criar_diretorio.c
+++ src/core/arquivo/criar_diretorio.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
+#include <sys/stat.h>
 
 static int _cria_diretorio(const char *nomeDiretorio) {
+    if (mkdir(nomeDiretorio, 0777) == 0) {
+        printf("Diretório criado: %s\n", nomeDiretorio);
+        return 0;
+    }
 
+    printf("Diretório já existe ou erro ao criar: %s\n", nomeDiretorio);
+    return 1;
 }
 
 int cria_diretorio(const char *nomeDiretorio) {
