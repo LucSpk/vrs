b13da3387ba820b5abd7cdf11840de4223914aa6
Lucas Alves <olalvezluc@gmail.com>
2026-03-16 18:59:27 -0300

    Remove identificador de arquivos

--- src/commands/start.c
+++ src/commands/start.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include "../../includes/core/criar_diretorio.h"
-#include "../../includes/core/identifica_arquivos.h"
 
 static int _cria_diretorios(char *dir) {
-
-    printf(dir);
-
     int err = 0;
     err = cria_diretorio(dir);
     if(err) {
@@ -26,14 +22,6 @@ int command_start(void) {
     for(int i = 0; i < 3; i++) {
         err = _cria_diretorios(dir[i]);
         if(err) return 1;
-    }
-
-    char **filePaths = NULL;
-    int totalArquivos = 0;
-    
-    identifica_arquivos(".", 0, &filePaths, &totalArquivos); 
-    for (int i = 0; i < totalArquivos; i++) {
-        printf("%s\n",filePaths[i]);
     }
 
     printf("Repositório iniciado com sucesso.\n");
