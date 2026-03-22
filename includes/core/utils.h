86a71f39e8222dfd16f1434b933bb3c80c22cc78
Lucas Alves <olalvezluc@gmail.com>
2026-03-22 19:49:24 -0300

    Cria o util para extrair substring

---
+++ includes/core/utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS
+#define UTILS
+
+char* extrairSubstring(const char* str, int inicio, int tamanho);
+
+#endif
---
+++ src/core/utils/extrai_substring.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+static char* _extrairSubstring(const char* str, int inicio, int tamanho) {
+    if (str == NULL || inicio < 0 || tamanho < 0) return NULL;
+    
+    char* sub = (char*)malloc(tamanho + 1); // +1 para o '\0'
+    if (sub == NULL) return NULL;
+    
+    strncpy(sub, str + inicio, tamanho);
+    sub[tamanho] = '\0'; // Garantir que a string termina
+    return sub;
+}
+
+char* extrairSubstring(const char* str, int inicio, int tamanho) {
+    return _extrairSubstring(str, inicio, tamanho);
+}
