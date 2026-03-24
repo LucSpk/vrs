81b80df4acc76bc90e277b0742aeca72b8e33a18
Lucas Alves <olalvezluc@gmail.com>
2026-03-23 22:15:05 -0300

    considera retorno em salva arquivo para sinalizar falha

--- includes/core/io.h
+++ includes/core/io.h
@@ -5,6 +5,6 @@
 
 int verifica(char *path);
 Arquivo le_conteudo_arquivo(const char *filePath);
-void salva_arquivo_no_diretorio(const char *caminho, const char *fileName, const unsigned char *zipFile);
+int salva_arquivo_no_diretorio(const char *caminho, const char *fileName, const unsigned char *zipFile);
 
 #endif
