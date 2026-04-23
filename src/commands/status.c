f4c1278266617dec5ccc170495df60a5615f1a70
Lucas Alves <olalvezluc@gmail.com>
2026-04-23 01:20:00 -0300

    Refactor _command_status to handle staged files and improve memory allocation

--- src/commands/status.c
+++ src/commands/status.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -13,10 +14,50 @@ int _command_status() {
     char path[1024];
     char status[32];
 
-    while (fgets(linha, sizeof(linha), fileIndex)) {
-        printf("%s\n", linha);
-    }
+    char **staged = malloc(10);
+    int tamanhoAtualStaged = 0;
+    int tamanhoStaged = 0;
+
+    char **modified = malloc(10);
+    int tamanhoAtualModified = 0;
+    int tamanhoModified = 0;
     
+    char **untracked = malloc(10); 
+    int tamanhoAtualUntracked = 0;
+    int tamanhoUntracked = 0;
+
+    while (fgets(linha, sizeof(linha), fileIndex)) {
+        hash[0] = '\0';
+        path[0] = '\0';
+        status[0] = '\0';
+
+        scanf(linha, "%s %s %s", hash, path, status);
+        
+        if(strcmp(status, "staged") == 0) {
+            if(tamanhoAtualStaged >= tamanhoStaged) {
+                tamanhoStaged *= 2;
+                char **temp = realloc(staged, tamanhoStaged);
+                if (!temp) {
+                    perror("Erro no realloc");
+                    return;
+                }
+                staged = temp;
+            }
+
+            int tamanhoResult = strlen(path) + 13;    // - "modified:   + <path> + \0"
+            char result[tamanhoResult]; 
+            sprintf(result, "modified:   %s", path);
+
+            (*staged)[tamanhoStaged] = malloc(tamanhoResult);
+            if(!(*staged)) {
+                printf("ERRO: Falha na alocação de memória (malloc retornou NULL)\n");
+                return 1;
+            }
+
+            sprintf((*staged)[tamanhoAtualStaged], result);
+            tamanhoAtualStaged++;
+        }
+    }
 
     return 0;
 }
