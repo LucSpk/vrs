d0aa83f7ff9e334015007d174cdc8dc5d3e52433
Lucas Alves <olalvezluc@gmail.com>
2026-04-23 00:56:27 -0300

    escaneia o index

--- src/commands/status.c
+++ src/commands/status.c
@@ -1,7 +1,26 @@
-int command_status(char *path) {
+#include <stdio.h>
+#include <string.h>
+
+int _command_status() {
+    FILE *fileIndex = fopen(".vsr/index", "r");
+    if(!fileIndex) {
+        printf("ERRO: Erro ao abrir o arquivo index.");
+        return 1;
+    }
+
+    char linha[1024];
+    char hash[128];
+    char path[1024];
+    char status[32];
+
+    while (fgets(linha, sizeof(linha), fileIndex)) {
+        printf("%s\n", linha);
+    }
+    
+
     return 0;
 }
 
-int command_status(char *path) {
-    return command_status(path);
+int command_status() {
+    return _command_status();
 }
