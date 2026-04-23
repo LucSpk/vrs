969f68b3b85cff9a18eff500cb4ebf55c23b4b3b
Lucas Alves <olalvezluc@gmail.com>
2026-04-23 01:28:51 -0300

    Refactor code structure for improved readability and maintainability

--- src/commands/status.c
+++ src/commands/status.c
@@ -14,16 +14,16 @@ int _command_status() {
     char path[1024];
     char status[32];
 
-    char **staged = malloc(10);
-    int tamanhoAtualStaged = 0;
+    int tamanhoAtualStaged = 10;
+    char **staged = malloc(sizeof(char *) * tamanhoAtualStaged);
     int tamanhoStaged = 0;
 
-    char **modified = malloc(10);
-    int tamanhoAtualModified = 0;
+    int tamanhoAtualModified = 10;
+    char **modified = malloc(sizeof(char *) * tamanhoAtualModified);
     int tamanhoModified = 0;
     
-    char **untracked = malloc(10); 
-    int tamanhoAtualUntracked = 0;
+    int tamanhoAtualUntracked = 10;
+    char **untracked = malloc(sizeof(char *) * tamanhoAtualUntracked); 
     int tamanhoUntracked = 0;
 
     while (fgets(linha, sizeof(linha), fileIndex)) {
@@ -31,15 +31,15 @@ int _command_status() {
         path[0] = '\0';
         status[0] = '\0';
 
-        scanf(linha, "%s %s %s", hash, path, status);
+        sscanf(linha, "%s %s %s", hash, path, status);
         
         if(strcmp(status, "staged") == 0) {
             if(tamanhoAtualStaged >= tamanhoStaged) {
                 tamanhoStaged *= 2;
-                char **temp = realloc(staged, tamanhoStaged);
+                char **temp = realloc(staged, sizeof(char *) * tamanhoStaged);
                 if (!temp) {
                     perror("Erro no realloc");
-                    return;
+                    return 1;
                 }
                 staged = temp;
             }
@@ -48,16 +48,17 @@ int _command_status() {
             char result[tamanhoResult]; 
             sprintf(result, "modified:   %s", path);
 
-            (*staged)[tamanhoStaged] = malloc(tamanhoResult);
+            staged[tamanhoAtualStaged] = malloc(sizeof(char *) * tamanhoResult);
             if(!(*staged)) {
                 printf("ERRO: Falha na alocação de memória (malloc retornou NULL)\n");
                 return 1;
             }
 
-            sprintf((*staged)[tamanhoAtualStaged], result);
+            strcpy(staged[tamanhoAtualStaged], result);
             tamanhoAtualStaged++;
         }
     }
+    
 
     return 0;
 }
