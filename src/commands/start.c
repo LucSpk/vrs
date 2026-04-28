bba68df4a3f64bc73163f48741d9a391f8a25cfb
Lucas Alves <olalvezluc@gmail.com>
2026-04-28 19:09:34 -0300

    Salva progresso, os diretorios estão sendo criados, mas segmentation fault ao criar os files

--- src/commands/start.c
+++ src/commands/start.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include "../../includes/core/criar_diretorio.h"
@@ -16,13 +17,28 @@ static int _cria_diretorios(char *dir) {
 int command_start(void) {
     printf("Inicializando repositório VSR...\n");
 
-    char *dir[3] = {".vsr", ".vsr/objects", ".vsr/logs", ".vsr/refs", ".vsr/refs/heads"};
+    char *dir[5] = {".vsr", ".vsr/objects", ".vsr/logs", ".vsr/refs", ".vsr/refs/heads"};
     int err = 0;
 
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < 5; i++) {
         err = _cria_diretorios(dir[i]);
         if(err) return 1;
     }
+
+    printf("cria arquivos\n");
+
+    // - Cria HEAD e aponta para a "main"
+    FILE *head = fopen(".vrs/HEAD", "w");
+    fprintf(head, "ref: refs/heads/main\n");
+    fclose(head);
+
+    // - Cria main
+    FILE *main = fopen(".vsr/refs/heads/main", "w");
+    fclose(main);
+
+    // - Cria index
+    FILE *index = fopen(".vrs/index", "w");
+    fclose(index);
 
     printf("Repositório iniciado com sucesso.\n");
 
