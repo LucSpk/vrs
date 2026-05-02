3ee919de1f4b0e7b16760bd7786c3ac0cd2b5c9b
Lucas Alves <olalvezluc@gmail.com>
2026-05-01 22:49:07 -0300

    define acoes para o comando

--- src/commands/ramify.c
+++ src/commands/ramify.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 static int _command_ramify(char branchName[]) {
-    printf("Ramificacao criado com sucesso!");
+    // 1. Verifica se ramificação ja existe nos refs
+    // 2. Caso exista dê um erro "Ramificação ja existe"
+    // 3. Caso não exista crie a ref com o nome da nova branch e o commit anterior da branch atual
+    // 4. Muda o ref para qual o HEAD aponta
+
     return 0;
 }
 
