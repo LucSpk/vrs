5edfdd0dc5caec953036907732af6f115037be37
Lucas Alves <olalvezluc@gmail.com>
2026-05-02 23:53:08 -0300

    Adiciona escopo do comando

--- src/commands/change.c
+++ src/commands/change.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 static int _command_change(char branchName[]) {
-
+    // 1. Verifica se branch existe na ref
+    // 2. Caso não exita lance um erro
+    // 3. Caso exista altere o conteudo do HEAD para apontar a ref
     return 0;
 }
 
