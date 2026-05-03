a87c95f10a780c87dc5e8b3ca99f1dcd04ccc8cc
Lucas Alves <olalvezluc@gmail.com>
2026-05-02 23:55:46 -0300

    verifica se branch destino existe

--- src/commands/change.c
+++ src/commands/change.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+#include "../../includes/core/io.h"
 
 static int _command_change(char branchName[]) {
     // 1. Verifica se branch existe na ref
+    char path[] = "./.vsr/refs/heads/";
+    strcat(path, branchName);
+
+    int err = 0;
+    err = verifica(path);
+    if(err) {
+        // 2. Caso exista dê um erro "Ramificação ja existe"
+        printf("Ramificação não existe");
+        return 1;
+    }
+
     // 2. Caso não exita lance um erro
     // 3. Caso exista altere o conteudo do HEAD para apontar a ref
     return 0;
