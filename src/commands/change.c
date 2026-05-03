5167eefea8072d7efc6d452631ae360ee58c7311
Lucas Alves <olalvezluc@gmail.com>
2026-05-02 23:57:44 -0300

    Troca de brach

--- src/commands/change.c
+++ src/commands/change.c
@@ -11,13 +11,16 @@ static int _command_change(char branchName[]) {
     int err = 0;
     err = verifica(path);
     if(err) {
-        // 2. Caso exista dê um erro "Ramificação ja existe"
+        // 2. Caso não exita lance um erro
         printf("Ramificação não existe");
         return 1;
     }
 
-    // 2. Caso não exita lance um erro
     // 3. Caso exista altere o conteudo do HEAD para apontar a ref
+    FILE *headWriteFile = fopen("./.vsr/HEAD", "w");
+    fprintf(headWriteFile, "ref: refs/heads/%s\n", branchName);
+
+    fclose(headWriteFile);
     return 0;
 }
 
