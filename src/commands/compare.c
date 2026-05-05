c9fd25736c40f16c58876336f1c8147bdbcb8dd2
Lucas Alves <olalvezluc@gmail.com>
2026-05-04 21:49:24 -0300

    Adiciona arquivo para comando compare

---
+++ src/commands/compare.c
@@ -0,0 +1,7 @@
+static int _command_compare(char objeto_a[], char objeto_b[]) {
+    return 0;
+}
+
+int command_compare(char objeto_a[], char objeto_b[]) {
+    return _command_compare(objeto_a, objeto_b);
+}
