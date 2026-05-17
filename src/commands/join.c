74eb968a200e653243b45f1727dec9bfc1f139c2
Lucas Alves <olalvezluc@gmail.com>
2026-05-17 11:12:20 -0300

    adiciona passos

--- src/commands/join.c
+++ src/commands/join.c
@@ -1,4 +1,21 @@
 static int _command_join(char *destino) {
+    // 1. Verifica se a branch destino existe
+    // 2. Pega o hash do ultimo commit da brancg destino
+    // 3. Le o HEAD
+    // 4. Pega o hash do ultimo commit da HEAD
+    // 5. Le os commits das branches
+    // 6. Le as trees dos commits
+    // 7. Compara os arquivos: novo, alterado e deletado
+    //       arquivo novo	                    adiciona
+    //       arquivo deletado                   apaga
+    //       arquivo alterado só numa branch	aceita
+    //       arquivo alterado nas duas	        conflito
+    // 8. Criar novo commit merge
+    //        |  tree <nova_tree>
+    //        |  parent <commit_main>
+    //        |  parent <commit_branch>
+    //      merge commit possui 2 parents
+    // 9. Atualizar main
     return 0;
 }
 
