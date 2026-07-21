bba91df5816b5a0d2e561f9e471d1fe2e6034e9d
Lucas Alves <olalvezluc@gmail.com>
2026-07-21 19:38:53 -0300

    Refatora função _command_ramify para delegar a verificação de ramificação e criação de arquivo à função command_ramify_from, simplificando a lógica de criação de ramificações.

--- src/commands/ramify.c
+++ src/commands/ramify.c
@@ -4,25 +4,6 @@
 #include "../../includes/core/io.h"
 
 static int _command_ramify(char branchName[]) {
-    // 1. Verifica se ramificação ja existe nos refs
-    
-    char path[] = "./.vsr/refs/heads/";
-    strcat(path, branchName);
-
-    int err = 0;
-    err = verifica(path);
-    if(!err) {
-        // 2. Caso exista dê um erro "Ramificação ja existe"
-        printf("Ramificação ja existe");
-        return 1;
-    }
-
-    FILE *newBranchFile = fopen(path, "w");
-    if(newBranchFile == NULL) {
-        printf("Erro: Não foi possível criar o arquivo: %s\n", path);
-        return 1;
-    }
-
     // 3. Caso não exista crie a ref com o nome da nova branch e o commit anterior da branch atual
     FILE *headFile = fopen("./.vsr/HEAD", "r");
     if(headFile == NULL) {
@@ -51,24 +32,11 @@ static int _command_ramify(char branchName[]) {
     parentHash[strcspn(parentHash, "\n")] = '\0';
     fclose(refFile);
 
-    fprintf(newBranchFile, "%s", parentHash);
-    fclose(newBranchFile);
-
-    // 4. Muda o ref para qual o HEAD aponta
-    FILE *headWriteFile = fopen("./.vsr/HEAD", "w");
-    fprintf(headWriteFile, "ref: refs/heads/%s\n", branchName);
-
-    fclose(headWriteFile);
-    return 0;
-}
-
-int command_ramify(char branchName[]) {
-    return _command_ramify(branchName);
+    return command_ramify_from(branchName, parentHash);
 }
 
 int command_ramify_from(char branchName[], char parentHash[]) {
     // 1. Verifica se ramificação ja existe nos refs
-    
     char path[] = "./.vsr/refs/heads/";
     strcat(path, branchName);
 
@@ -95,4 +63,8 @@ int command_ramify_from(char branchName[], char parentHash[]) {
 
     fclose(headWriteFile);
     return 0;
+}
+
+int command_ramify(char branchName[]) {
+    return _command_ramify(branchName);
 }
